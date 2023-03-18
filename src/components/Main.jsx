de7adffbd18e325742c6ef5abcffd235ef01d753
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-03-17 22:47:21 -0600

    added homescreen with api call and background of random movie

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,70 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import requests from "../Requests";
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
+  const [showFullText, setShowFullText] = useState(false);
 
-  return <div></div>;
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
+  useEffect(() => {
+    axios.get(requests.requestList).then((response) => {
+      setMovies(response.data.items);
+    });
+  }, []);
+
+  const toggleFullText = () => {
+    setShowFullText(!showFullText);
+  };
+
+  const truncateString = (str, num) => {
+    if (str?.length > num && !showFullText) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
+
+  return (
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <img
+          className="w-full h-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
+          <div className="py-4">
+            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+              Play
+            </button>
+            <button className="border text-white border-gray-300 py-2 px-5 ml-4">
+              Watch Later
+            </button>
+          </div>
+          <p className="text-gray-400 text-sm">
+            Released:{" "}
+            {new Date(movie?.release_date).toLocaleDateString("en-US", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })}
+          </p>
+
+          <p
+            className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200"
+            onClick={toggleFullText}
+          >
+            {truncateString(movie?.overview, 150)}
+            {showFullText && truncateString(movie?.overview.slice(150), 999999)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
