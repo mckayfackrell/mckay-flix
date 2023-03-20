702acdcda13ec81cbcbf23d374986be8bfad20e4
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-03-20 17:45:23 -0600

    changed styling

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -54,7 +54,7 @@ const Movie = ({ item }) => {
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
-        <div className="absolute w-full top-[20%] p-4 md:p-8">
+        <div className="absolute w-full top-[10%] p-4 md:p-8">
           <h1 className="text-xl md:text-5xl font-bold">{movie?.title}</h1>
           <div className="py-4 flex items-center">
             <p className="mr-4" onClick={saveShow}>
