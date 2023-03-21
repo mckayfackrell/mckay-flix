1621f2d2ac59c3f512c3f9d42f8b1e3650ce9fa5
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-03-20 21:55:07 -0600

    Update Home.jsx added TV Shows row

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,33 +12,39 @@ const Home = () => {
         title="McKay's Favorites"
         fetchURL={requests.requestMcKay}
       />
-      <Row rowID="2" title="Harry Potter" fetchURL={requests.requestPotter} />
+        <Row rowID="2" title="Harry Potter" fetchURL={requests.requestPotter} />
       <Row
         rowID="3"
+        title="TV Shows"
+        fetchURL={requests.requestTV}
+      />
+      <Row rowID="4" title="Harry Potter" fetchURL={requests.requestPotter} />
+      <Row
+        rowID="5"
         title="Jurassic Park"
         fetchURL={requests.requestJurassic}
       />
-      <Row rowID="4" title="Star Wars" fetchURL={requests.requestStar} />
-      <Row rowID="5" title="Studio Ghibli" fetchURL={requests.requestGhibli} />
+      <Row rowID="6" title="Star Wars" fetchURL={requests.requestStar} />
+      <Row rowID="7" title="Studio Ghibli" fetchURL={requests.requestGhibli} />
       <Row
-        rowID="6"
+        rowID="8"
         title="Christopher Nolan"
         fetchURL={requests.requestNolan}
       />
-      <Row rowID="7" title="Audrey Hepburn" fetchURL={requests.requestAudrey} />
+      <Row rowID="9" title="Audrey Hepburn" fetchURL={requests.requestAudrey} />
       <Row
-        rowID="8"
+        rowID="10"
         title="The Lord of the Rings"
         fetchURL={requests.requestRings}
       />
-      <Row rowID="9" title="X-Men" fetchURL={requests.requestX} />
+      <Row rowID="11" title="X-Men" fetchURL={requests.requestX} />
       <Row
-        rowID="10"
+        rowID="12"
         title="Planet of the Apes"
         fetchURL={requests.requestApes}
       />
-      <Row rowID="11" title="Christmas" fetchURL={requests.requestChristmas} />
-      <Row rowID="12" title="Horror" fetchURL={requests.requestHorror} />
+      <Row rowID="13" title="Christmas" fetchURL={requests.requestChristmas} />
+      <Row rowID="14" title="Horror" fetchURL={requests.requestHorror} />
     </div>
   );
 };
