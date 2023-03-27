7ec90b635d9858e1ef4af386130f8e7238366638
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-03-27 17:19:13 -0600

    updated height of movie backdrop

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -46,9 +46,9 @@ const Movie = ({ item }) => {
   }, [id]);
 
   return (
-    <div className="w-full h-[250px] md:h-[550px] text-white">
+    <div className="w-full h-[250px] md:h-[350px] text-white">
       <div className="w-full h-full">
-        <div className="absolute w-full h-[250px] md:h-[550px] bg-gradient-to-r from-black"></div>
+        <div className="absolute w-full h-[250px] md:h-[350px] bg-gradient-to-r from-black"></div>
         <img
           className="w-full h-full object-cover"
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
